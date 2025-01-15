64a0c7ee87127f966c0375ca112ebafe7c71c72e
Tycho Engberink <tycho.engberink@dij.digital>
2025-01-15 16:50:40 +0100

    Fix non static function in callback of migration

--- database/migrations/2018_08_08_100000_create_telescope_entries_table.php
+++ database/migrations/2018_08_08_100000_create_telescope_entries_table.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use MongoDB\Laravel\Schema\Blueprint;
 
 return new class extends Migration
 {
@@ -21,7 +21,7 @@ return new class extends Migration
     {
         $schema = Schema::connection($this->getConnection());
 
-        $schema->create('telescope_entries', function (Blueprint $collection){
+        $schema->create('telescope_entries', static function (Blueprint $collection) {
             $collection->unique('uuid');
             $collection->index('batch_id');
             $collection->index('family_hash');
